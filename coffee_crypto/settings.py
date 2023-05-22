5520cc85a85353140cc06998a4046d665a084b67
pdhdfc <pdhdfc@gmail.com>
2023-05-23 01:59:40 +0530

    Krishna

--- coffee_crypto/settings.py
+++ coffee_crypto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--6-t@n6quz++sysh(fkzm4l#zos@euqi!*1sws2&dw#zshhgxp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
